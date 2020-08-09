114a39c5a12362021d340030c37bc644091cf9c2
bbbnam <bbbnam@naver.com>
2020-08-10 01:40:42 +0900

    feat : 입국심사 문제풀이

--- src/main/java/programmers/입국심사/Immigration.java
+++ src/main/java/programmers/입국심사/Immigration.java
@@ -1,8 +1,18 @@
 package programmers.입국심사;
 
+import java.util.*;
+
 public class Immigration {
     public long solution(int n, int[] times) {
         long answer = 0;
+        Arrays.sort(times);
+        long maxTime = n * times[times.length-1]; //가능한 최대시간
+        answer = findMinTime();
+
         return answer;
+    }
+
+    private long findMinTime() {
+        //반 가르기 시작
     }
 }
