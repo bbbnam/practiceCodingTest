0d96c3a4375ec3091be768a7f3a524d3204d6f4a
bbbnam <bbbnam@naver.com>
2020-08-10 23:47:16 +0900

    feat : 입국심사 문제 풀이

--- src/main/java/programmers/입국심사/Immigration.java
+++ src/main/java/programmers/입국심사/Immigration.java
@@ -4,15 +4,47 @@ import java.util.*;
 
 public class Immigration {
     public long solution(int n, int[] times) {
-        long answer = 0;
         Arrays.sort(times);
-        long maxTime = n * times[times.length-1]; //가능한 최대시간
-        answer = findMinTime();
+        return BinarySearch(n, times);
+    }
 
+    private long BinarySearch(int n, int[] times) {
+        long minTime = 1;
+        long slower = times[times.length-1]; // 이 부분 이렇게 변수로 안빼면 시간때문에 걸리는듯..
+        long maxTime = n * slower; //가능한 최대시간
+        long mid = 0;
+        long answer = maxTime;
+
+        while (minTime <= maxTime){
+            mid = (minTime + maxTime) / 2;
+
+            if ( isSmallNode(times, mid, n)) {
+                answer = answer > mid ? mid : answer;
+                maxTime = mid - 1;
+            }  else {
+                minTime = mid + 1;
+            }
+        }
         return answer;
     }
 
-    private long findMinTime() {
-        //반 가르기 시작
+    private boolean isSmallNode(int[] times, long mid, long n) {
+        long sum = 0;
+
+        for (int time : times) {
+            sum += mid / time;
+        }
+
+        if (sum >= n) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Immigration immigration = new Immigration();
+        System.out.println(">"+immigration.solution(6, new int[]{7,10})); // 28
+        System.out.println(">"+immigration.solution(5, new int[]{2,5,6})); // 6
     }
 }
