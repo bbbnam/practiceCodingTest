ccbf89d463b3c3d87d775044e49a5c4b3b4c44c7
bbbnam <bbbnam@naver.com>
2021-09-27 19:43:20 +0900

    feat : 음수 뒤로 보내서 합계가 음수가 되지 않게끔 처리 테스트 (배열의 총합은 음수가 아닌 것은 보장됨)

---
+++ src/main/java/practice/NoNegative.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class NoNegative {
+
+    public static void main(String[] args) {
+        NoNegative noNegative = new NoNegative();
+        System.out.println(noNegative.solution(new int[] {-1, -1, 1, 1, 1}));
+        //System.out.println(noNegative.solution(new int[] {10, -10, -1, -1, 10}));
+        //System.out.println(noNegative.solution(new int[] {-1, -1, -1, 1, 1, 1, 1}));
+        //System.out.println(noNegative.solution(new int[] {5, -2, -3, 1}));
+    }
+
+    public int solution(int[] A) {
+        int count = 0;
+
+        return count;
+    }
+}
