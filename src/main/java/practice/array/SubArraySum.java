82cd9f76003a3119ce7a8d1026c18d1bbfa10425
bbbnam <bbbnam@naver.com>
2021-10-10 22:25:06 +0900

    feat : 순서대로 더해갈 때 목표값 나오는 부분집합 갯수 찾기(map 방식풀이)

--- src/main/java/practice/array/SubArraySum.java
+++ src/main/java/practice/array/SubArraySum.java
@@ -6,16 +6,20 @@ package practice.array;
 출력 : 4
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArraySum {
     public static void main(String[] args) {
         int[] nums = new int[]{3, 4, 7, 2, -3, 1, 4, 2};
         int k = 7;
         SubArraySum subArraySum = new SubArraySum();
-        System.out.println(subArraySum.solution(nums, k));
+        //System.out.println(subArraySum.solution_byArray(nums, k));
+        System.out.println(subArraySum.solution_byMap(nums, k));
     }
 
     // 1. 이중 for
-    private int solution(int[] nums, int k) {
+    private int solution_byArray(int[] nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             int sum = 0;
@@ -27,6 +31,26 @@ public class SubArraySum {
                     count++;
                 }
             }
+        }
+
+        return count;
+    }
+
+    // 2. map
+    private int solution_byMap(int[] nums, int k) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+
+        // 2
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i]; // sum = sum + nums[i]
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
