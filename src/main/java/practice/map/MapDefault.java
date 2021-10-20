d2158a1562c7b387f7774750f39b25c7e172fbfe
bbbnam <bbbnam@naver.com>
2021-10-20 22:39:56 +0900

    feat : Map get 하는법 기본 연습

---
+++ src/main/java/practice/map/MapDefault.java
@@ -0,0 +1,33 @@
+package practice.map;
+
+import java.util.*;
+
+public class MapDefault {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "A");
+        map.put(2, "B");
+        map.put(3, "C");
+
+        System.out.println("=========1==============");
+        // 1
+        for (Integer number : map.keySet()) {
+            System.out.println("key : " + number + ", value : " + map.get(number));
+        }
+
+        System.out.println("=========2==============");
+        // 2
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("key : " + entry.getKey() + ", value : " + entry.getValue());
+        }
+
+        System.out.println("=========3==============");
+        // 3
+        Iterator<Integer> iterator = map.keySet().iterator();
+        while(iterator.hasNext()) {
+            Integer key = iterator.next();
+            System.out.println("key : " + key + ", value : " + map.get(key));
+        }
+    }
+}
