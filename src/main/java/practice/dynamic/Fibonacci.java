c30630d4a76768cd2f4892337fb3851c49fd9104
bbbnam <bbbnam@naver.com>
2021-11-03 21:52:51 +0900

    feat : 피보나치 수열

---
+++ src/main/java/practice/dynamic/Fibonacci.java
@@ -0,0 +1,19 @@
+package practice.dynamic;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            System.out.println("호출전 i: "+i+" value : "+fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int n) {
+
+        System.out.println("fibonacci 호출 :"+n);
+        if (n <= 1)
+            return n;
+        else
+            return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+}
