fb361a2ea880ef61d66fb4d99caa2a74f2479ed4
bbbnam <bbbnam@naver.com>
2021-11-04 19:39:24 +0900

    feat : 피보나치 수열 (top_down)

---
+++ src/main/java/practice/dynamic/Fibonacci_top_down.java
@@ -0,0 +1,25 @@
+package practice.dynamic;
+
+public class Fibonacci_top_down {
+    public static void main(String[] args) {
+        Fibonacci_top_down a = new Fibonacci_top_down();
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            System.out.println("1_호출전 i: " + i + " value : " + a.fibonacci(i));
+        }
+    }
+
+
+    public int fibonacci(int n) {
+        int[] bottomUp = new int[1000];
+        System.out.println("2_fibonacci 호출 :" + n);
+
+        bottomUp[1] =1;
+        bottomUp[2] =1;
+
+        for(int i=3; i<=n; i++) {
+            bottomUp[i] = bottomUp[i-1] + bottomUp[i-2];
+        }
+        return bottomUp[n];
+    }
+}
