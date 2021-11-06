e0cdba3bcab8ad4d9581b44b1d8ace906005a092
bbbnam <bbbnam@naver.com>
2021-11-06 23:07:45 +0900

    refactor : change String format

--- src/main/java/practice/dynamic/Fibonacci_top_down.java
+++ src/main/java/practice/dynamic/Fibonacci_top_down.java
@@ -5,14 +5,14 @@ public class Fibonacci_top_down {
         Fibonacci_top_down a = new Fibonacci_top_down();
         int n = 7;
         for (int i = 1; i <= n; i++) {
-            System.out.println("1_호출전 i: " + i + " value : " + a.fibonacci(i));
+            System.out.println("1 호출전 i: " + i + " value : " + a.fibonacci(i));
         }
     }
 
 
     public int fibonacci(int n) {
         int[] bottomUp = new int[1000];
-        System.out.println("2_fibonacci 호출 :" + n);
+        System.out.println("2 fibonacci 호출 :" + n);
 
         bottomUp[1] =1;
         bottomUp[2] =1;
