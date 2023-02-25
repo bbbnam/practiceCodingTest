0b3f5b4c0daf42c4630879e047563555b2b8fe6a
jr <jr@gccompany.co.kr>
2023-02-25 21:41:57 +0900

    feat : 기호상수 public 선언하여 리팩토링 추가

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -1,10 +1,12 @@
 package refactoring;
 
+import static refactoring.AF_MyRefactoringRobot.*;
+
 public class Main {
     public static void main(String[] args) {
         BF_RefactoringRobot robot_1 = new BF_RefactoringRobot("Andrew");
-        robot_1.order(0); // walk
-        robot_1.order(1); // stop
-        robot_1.order(2); // jump
+        robot_1.order(WALKS_COMMAND); // walk
+        robot_1.order(STOPS_COMMAND); // stop
+        robot_1.order(JUMPS_COMMAND); // jump
     }
 }
