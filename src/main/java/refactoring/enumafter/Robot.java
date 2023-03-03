e40aebf61ff706a07374f19355eaa5018e36d218
jr <jr@gccompany.co.kr>
2023-03-03 22:28:56 +0900

    refactor : 주석2

--- src/main/java/refactoring/enumafter/Robot.java
+++ src/main/java/refactoring/enumafter/Robot.java
@@ -2,7 +2,7 @@ package refactoring.enumafter;
 
 public class Robot {
     private final String _name;
-    public enum Command {
+    public enum Command { // 이 enum은 별도로 두는게 어떤지?
         WALK,
         STOP,
         JUMP
@@ -15,7 +15,7 @@ public class Robot {
     //이런식으로 파라미터를 받아야 컴파일 시 에러 발견이 용이하다
     public void order(Robot.Command command) {
         if (command == Command.WALK) {
-            System.out.println(_name + " walks."); //if문을 줄일 방법은?
+            System.out.println(_name + " walks.");
         } else if (command == Command.STOP) {
             System.out.println(_name + " stops.");
         } else if (command == Command.JUMP) {
