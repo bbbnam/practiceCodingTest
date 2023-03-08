cdf18c9c8428a0b0b248d8084067242e57319b4c
jr <jr@gccompany.co.kr>
2023-03-08 23:25:10 +0900

    feat : 챕터 2 리팩토링 후(break 사용)

---
+++ src/main/java/refactoring/chapter2/afterBreak/FindInt.java
@@ -0,0 +1,14 @@
+package refactoring.chapter2.afterBreak;
+
+public class FindInt {
+    public static boolean find(int[] data, int target) {
+        boolean found = false;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+}
