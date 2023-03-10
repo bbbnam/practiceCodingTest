78669cd8a0635d33112434e6417f693c176a9f15
jr <jr@gccompany.co.kr>
2023-03-10 23:28:57 +0900

    feat : 리팩토링 return 사용으로 변경

---
+++ src/main/java/refactoring/chapter2/afterReturn/FindInt.java
@@ -0,0 +1,12 @@
+package refactoring.chapter2.afterReturn;
+
+public class FindInt {
+    public static boolean find(int[] data, int target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
